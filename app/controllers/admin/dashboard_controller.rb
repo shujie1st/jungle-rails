e11f8c43d1e75c5eb8df6611a8f333dc04df1960
Shujie Ma <shujie1st@gmail.com>
2023-07-05 21:19:31 -0400

    Implement feature: dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
   
   def show
+    @product_count = Product.all.length
+    @category_count = Category.all.length
   end
 end
